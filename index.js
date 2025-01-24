288b8403aad1ac3cf6e90c84971c1f3545eeb14c
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-24 20:32:22 +0500

    8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // const {printError, printSuccess} = require('./services/log.service.js')
 import getArgs from './helpers/args.js'
 import {printError, printSuccess, printHelp} from './services/log.service.js'
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+import { saveKeyValue, getKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
 import {getWeather} from './services/api.service.js'
 
 const saveToken = async token => {
@@ -18,9 +18,24 @@ const saveToken = async token => {
     }
 }
 
+const saveCity = async city => {
+    if(!city.length) {
+        printError("City doesn't exist")
+        return
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city)
+        printSuccess('City was saved')
+    } catch (err) {
+        printError(err.message)
+    }
+}
+
 const getForcast = async () => {
     try {
-        const response = await getWeather(process.env.CITY ?? 'Uzbekistan')
+        const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city))
+        // const response = await getWeather(process.env.CITY ?? 'Uzbekistan')
+        const response = await getWeather(city)
         console.log(response)
     } catch (err) {
         if(err?.response?.status == 404) {
@@ -46,10 +61,11 @@ const startCLI = () => {
     // console.log(process.env)
     if(args.h) {
         // help
-        printHelp()
+        return printHelp()
     }
     if(args.s) {
         // save city
+        return saveCity(args.s)
     }
     if(args.t) {
         // save token
@@ -57,7 +73,7 @@ const startCLI = () => {
         return saveToken(args.t)
     }
     // result
-    getForcast()
+    return getForcast()
 }
 
 startCLI()
