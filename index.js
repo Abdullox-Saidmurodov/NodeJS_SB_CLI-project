75085c9c3b24df9232f881c48f6cb9b9506226c2
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-24 17:56:22 +0500

    4

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const getArgs = require('./helpers/args')
+// const getArgs = require('./helpers/args')
+// const {printError, printSuccess} = require('./services/log.service.js')
+import getArgs from './helpers/args.js'
+import {printError, printSuccess, printHelp} from './services/log.service.js'
+import { saveKeyValue } from './services/storage.service.js'
+
+const saveToken = async token => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Token was saved')
+    } catch (err) {
+        printError(err.message)
+    }
+}
 
 const startCLI = () => {
     // console.log("start")
@@ -8,14 +21,19 @@ const startCLI = () => {
 
     // console.log(process.argv)
 
+    // printSuccess('ok')
+    // printError('no ok')
     if(args.h) {
         // help
+        printHelp()
     }
     if(args.s) {
         // save city
     }
     if(args.t) {
         // save token
+        // saveKeyValue('token', args.t)
+        return saveToken(args.t)
     }
     // result
 }
---
+++ services/storage.service.js
@@ -0,0 +1,39 @@
+import os from 'os'
+import path from 'path'
+import fs from 'fs'
+
+const filePath = path.join(path.join(os.homedir(), 'weather-date.json'))
+
+const saveKeyValue = async (key, value) => {
+    // console.log(path.join(os.homedir(), 'weather-date.json'))
+    let data = {}
+
+    if(await isExist(filePath)) {
+        const file = await fs.promises.readFile(filePath)
+        data = JSON.parse(file)
+    }
+
+    data[key] = value
+    await fs.promises.writeFile(filePath, JSON.stringify(data))
+}
+
+const getKeyValue = async key => {
+    if(await isExist(filePath)) {
+        const file = await fs.promises.readFile(filePath)
+        const date = JSON.parse(file)
+        return data[key]
+    }
+
+    return undefined
+}
+
+const isExist = async path => {
+    try {
+        await fs.promises.stat(path)
+        return true
+    } catch(err) {
+        return false
+    }
+}
+
+export {saveKeyValue, getKeyValue}
